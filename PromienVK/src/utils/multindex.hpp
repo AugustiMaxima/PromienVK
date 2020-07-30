fec707e3433256ebaf909c25b0db4b6c2525f8a2
Lennox Fei <f5fei@uwaterloo.ca>
2020-07-30 03:43:30 -0400

    imagine wasting this much work on a meme feature of vulkan queues

--- PromienVK/src/utils/multindex.hpp
+++ PromienVK/src/utils/multindex.hpp
@@ -84,9 +84,9 @@ namespace util {
 
 
 		public:
-			iNode(K key, V value, iNode* parent = nullptr) :key(key), value(value), p(parent), height(1), l(nullptr), r(nullptr) {
+			iNode(const K& key, V value, iNode* parent = nullptr) :key(key), value(value), p(parent), height(1), l(nullptr), r(nullptr) {
 			}
-			iNode* find(K key) {
+			iNode* find(const K& key) {
 				if (key < this->key) {
 					if (l)
 						return l->find(key);
@@ -103,31 +103,37 @@ namespace util {
 						return r;
 				}
 			}
-			//A word on put: update as a function is only guaranteed for single indexes, don't mix mult and update
-			iNode* put(K key, V value, V* residual) {
+			iNode* query(const K& key, int direction = 1) {
 				if (key < this->key) {
-					if (l) 
-						l = l->put(key, value, residual);
+					if (l)
+						return l->query(key, direction);
 					else {
-						l = new iNode(key, value, this);
-						*residual = value;
+						if (direction < 0) {
+							return this;
+						}
+						else {
+							return this->right();
+						}
 					}
 				}
 				else if (key == this->key) {
-					*residual = this->value;
-					this->value = value;
+					return this;
 				}
 				else {
 					if (r)
-						r = r->put(key, value, residual);
+						return r->query(key, direction);
 					else {
-						r = new iNode(key, value, this);
-						*residual = value;
+						if (direction > 0) {
+							return this;
+						}
+						else {
+							return this->left();
+						}
 					}
 				}
-				return balance();
 			}
-			iNode* put(K key, V value) {
+			//A word on put: update as a function is only guaranteed for single indexes, don't mix mult and update
+			iNode* put(const K& key, V value) {
 				if (key < this->key) {
 					if (l)
 						l = l->put(key, value);
@@ -147,7 +153,7 @@ namespace util {
 				}
 				return balance();
 			}
-			iNode* insert(K key, V value) {
+			iNode* insert(const K& key, V value) {
 				if (key < this->key) {
 					l = l->insert(key, value);
 				}
@@ -159,8 +165,28 @@ namespace util {
 				}
 				return balance();
 			}
-			iNode* left();
-			iNode* right();
+			iNode* left() {
+				if (l)
+					return l;
+				iNode* ps = p;
+				iNode* c = this;
+				while (ps && ps->l == c) {
+					c = ps;
+					ps = ps->p;
+				}
+				return ps;
+			}
+			iNode* right() {
+				if (r)
+					return r;
+				iNode* ps = p;
+				iNode* c = this;
+				while (ps && ps->r == c) {
+					c = ps;
+					ps = ps->p;
+				}
+				return ps;
+			}
 			iNode* remove() {
 				iNode* rep = nullptr;
 				if (l && l->r) {
@@ -206,7 +232,7 @@ namespace util {
 				delete this;
 				return rep;
 			}
-			iNode* remove(K key) {
+			iNode* remove(const K& key) {
 				if (key < this->key) {
 					l = l->remove(key);
 				}
@@ -251,14 +277,28 @@ namespace util {
 		};
 
 		class iKey {
-			multIndex* index;
 			iNode* src;
-			friend multIndex<K, V>;
 		public:
 			iKey(iNode* src) :src(src) {
 			}
+			operator bool() {
+				return src;
+			}
 			operator V&() {
 				return src->getValue();
+			}
+			iKey operator+(int l){
+				iNode* s = src;
+				for (int i = 0; i < l; i++)
+					s = s->right();
+				return iKey(s);
+			}
+			iKey operator++() {
+				src = src->right();
+				return *this;
+			}
+			iKey operator++(int) {
+				return ++(*this);
 			}
 		};
 
@@ -266,19 +306,52 @@ namespace util {
 	public:
 		multIndex() :root(nullptr) {
 		}
-		V put(K key, V value);
-		std::optional<K> get(K key);
-		std::vector<K> query(K left, K right);
-		iKey select(K key);
-		iKey iterate(int direction);
+		void put(K key, V value) {
+			root->put(key, value);
+		}
+		std::optional<V> get(const K& key) {
+			iNode* kn = root->find(key);
+			if (kn)
+				return std::optional<V>(kn->getValue());
+			return std::optional<V>();
+		}
+		std::vector<V> query(const K& left, const K& right) {
+			iNode* l = root->query(left);
+			//edge, inclusive
+			iNode* r = root->query(right, -1);
+			//edge, inclusive
+			iNode* pb = l;
+			if (l) {
+				while (pb && pb->getKey() == left) {
+					l = pb;
+					pb = pb->left();
+				}
+			}
+			if (r) {
+				pb = r;
+				while (pb && pb->getKey() == right) {
+					r = pb;
+					pb = pb->right();
+				}
+				//right most limit, helps with the iterator end logic
+				r = r->right();
+			}
+			std::vector<V> results;
+			if (l) {
+				for (iNode* i = l; i != r; i = i->right()) {
+					results.push_back(i->getValue());
+				}
+			}
+			return results;
+		}
+		iKey select(const K& key) {
+			return iKey(root->find(key));
+		}
+		
 		~multIndex() {
 			delete root;
 		}
 	};
-
-
-
-
 
 }
 
