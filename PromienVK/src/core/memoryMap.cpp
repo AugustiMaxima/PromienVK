d697e04ee1cb8318ce4bc39ca7f87a92d9c38c3b
Lennox Fei <f5fei@uwaterloo.ca>
2020-08-28 20:05:59 -0400

    last mile before the move

--- PromienVK/src/core/memoryMap.cpp
+++ PromienVK/src/core/memoryMap.cpp
@@ -15,7 +15,24 @@ namespace core {
 
 		int vMemory::selectMemoryType(vk::PhysicalDevice device, int typeFilter, vk::MemoryPropertyFlags flag) {
 			vk::PhysicalDeviceMemoryProperties prop = device.getMemoryProperties();
-			
+			for (int i = 0; i < prop.memoryTypeCount; i++) {
+				if (typeFilter & (1 << i) && (prop.memoryTypes[i].propertyFlags & flag) == flag)
+					return i;
+			}
+		}
+
+		vPointer vMemory::malloc(int bytes) {
+			int offset = allocator.malloc(bytes);
+			allocRegistry.put(offset, true);
+			return vPointer(src, offset);
+		}
+
+		void vMemory::free(vPointer ptr) {
+			if (ptr.memory != src)
+				throw std::exception("This memory was allocated from a different heap of deviceMemory");
+			if (!allocRegistry.get(ptr.offset))
+				throw std::exception("No pointer of the correct offset found");
+			allocator.free(ptr.offset);
 		}
 	}
 }
--- PromienVK/src/core/memoryMap.hpp
+++ PromienVK/src/core/memoryMap.hpp
@@ -8,18 +8,21 @@ namespace core {
 	namespace ram {
 		//incomplete draft -- more to be done with interface & abstraction
 		struct vPointer {
-			vk::DeviceMemory memory;
-			int offset;
+			const vk::DeviceMemory memory;
+			const int offset;
 			vPointer(vk::DeviceMemory memory, int offset);
 		};
 
 		class vMemory {
 			vk::DeviceMemory src;
 			infr::lvm::LinearVM allocator;
+			util::multIndex<int, bool> allocRegistry; //An additional safety check to prevent bad free
 		public:
 			vMemory(vk::DeviceMemory src, int size);
 			static vMemory createMemoryPool(vk::Device device, int size, int memoryType);
 			static int selectMemoryType(vk::PhysicalDevice device, int typeFilter, vk::MemoryPropertyFlags flag);
+			vPointer malloc(int bytes);
+			void free(vPointer ptr);
 		};
 
 
